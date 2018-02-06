5014053c28a8b86013dfe23f2a2ae7dd0238df00
Newman Posey <newman@newmans-mbp.attlocal.net>
2018-02-06 13:31:16 -0500

    update

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Salmon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tofu Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', true);
